c259b93846abe00375968556add00d637acfae1c
Dmytrii Lukiashchenko <lukiaschenkoff@gmail.com>
2023-11-11 12:01:29 +0100

    Writed header and first chapter of the about page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./templates/**/*.{html.twig, html}"],
   theme: {
+    colors: {
+      'wcblue' : '#212738',
+      'wcwhite' : '#E8E8E8',
+      "white" : "#fff",
+      "black": "#000"
+    },
     extend: {},
+    fontFamily: {
+      "logo" : "Leckerli One",
+      "default" : "Lexend"
+    }
   },
   plugins: [],
 }
