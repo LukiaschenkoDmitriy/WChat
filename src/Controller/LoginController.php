50b5fcf2c4255ec5f5452e10152fbdb6649c9298
Dmytrii Lukiashchenko <lukiaschenkoff@gmail.com>
2023-11-13 00:11:57 +0100

    Initializated Doctrine, added lock on the login page

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,35 @@ use Symfony\Component\Routing\Annotation\Route;
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        if (key_exists("email", $_POST) && key_exists("password", $_POST)) {
+            $usersRepository = $entityManager->getRepository(User::class);
+            $account = $usersRepository->findOneBy([
+                "email" => $_POST["email"],
+                "password" => $_POST["password"]
+            ]);
+
+            if ($account == null) {
+                $checkOnEmail = $usersRepository->findOneBy([
+                    "email" => $_POST["email"]
+                ]);
+
+                if ($checkOnEmail == null) {
+                    return $this->render("login/index.html.twig", [
+                        "dataExist" => true
+                    ]);
+                }
+            }
+            else {
+                return $this->render("chat/index.html.twig", [
+                    "user" => $account
+                ]);
+            }
+        }
+
         return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
+            "dataExist" => false
         ]);
     }
 }
