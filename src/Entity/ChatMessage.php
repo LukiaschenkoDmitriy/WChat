9d38b7aa28fb1b8b9bb1ccb01f001e8d2a199ec7
Dmytrii Lukiashchenko <lukiaschenkoff@gmail.com>
2023-12-13 15:55:36 +0100

    Added new 'ChatMessage' table in to database

---
+++ src/Entity/ChatMessage.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ChatMessageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ChatMessageRepository::class)]
+class ChatMessage
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $user_id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $message = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $data = null;
+
+    #[ORM\Column]
+    private ?int $chat_id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(int $user_id): static
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): static
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getData(): ?string
+    {
+        return $this->data;
+    }
+
+    public function setData(string $data): static
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    public function getChatId(): ?int
+    {
+        return $this->chat_id;
+    }
+
+    public function setChatId(int $chat_id): static
+    {
+        $this->chat_id = $chat_id;
+
+        return $this;
+    }
+}
