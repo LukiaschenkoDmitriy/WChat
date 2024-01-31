768ac92becb3dea77ea38c0ac52629d1ef2a821a
Dmytrii Lukiashchenko <lukiaschenkoff@gmail.com>
2024-01-31 08:42:46 +0000

    Wrote html template && css

--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,8 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
-    .addEntry('login', "./assets/login.js")
+    // .addEntry('app', './assets/app.js')
+    .addEntry('autorization', "./assets/autorization.js")
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -59,6 +59,7 @@ Encore
 
     // uncomment if you use TypeScript
     .enableTypeScriptLoader()
+    .enablePostCssLoader()
 
     // uncomment if you use React
     //.enableReactPreset()
